1ee47a5af07385877434af232fdf4627621effc0
TheNeekOfficial <neekspare@proton.me>
2026-03-12 15:15:41 +1100

    initial setup

---
+++ flake.nix
@@ -0,0 +1,30 @@
+{
+  description = "A python template for pandas etc.";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+  };
+
+  outputs = {
+    self, # Necessary for direnv
+    nixpkgs,
+  }: let
+    forAllSystems = nixpkgs.lib.genAttrs ["x86_64-linux" "aarch64-darwin"];
+    pkgs = forAllSystems (system: nixpkgs.legacyPackages.${system});
+    pythonPackages = ps:
+      with ps; [
+        ipython
+        pandas
+        tkinter
+        python-lsp-server
+      ];
+  in {
+    devShells = forAllSystems (system: {
+      default = pkgs.${system}.mkShellNoCC {
+        packages = with pkgs.${system}; [
+          (python3.withPackages pythonPackages)
+        ];
+      };
+    });
+  };
+}
